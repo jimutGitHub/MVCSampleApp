fe86660bba25e5fde8e5e4a1c1208fb9985ee562
jimutGitHub <JimutGitHub@users.noreply.github.com>
2019-01-25 14:44:21 +0530

    Countries List view added.

---
+++ MVCSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "India",
+                "US",
+                "Austrelia"
+            };
+            return View();
+        }
+    }
+}
